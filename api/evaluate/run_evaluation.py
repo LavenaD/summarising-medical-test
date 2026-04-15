486be881aa6cb2498f4cd1c673a6a97624b0009f
LavenaD <lavena.dmello@gmail.com>
2026-04-15 23:26:27 +0100

    changes to evaluation

---
+++ api/evaluate/run_evaluation.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import torch
+import evaluate
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from peft import PeftModel
+
+def run_evaluation_job(job_id):
+    model_path = r"api\\django_api\\ai_model"
+
+    # load base model
+    base_model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-small")
+
+    # attach LoRA adapter
+    model = PeftModel.from_pretrained(base_model, model_path)
+
+    # tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+
+    device = torch.device("cpu")
+    model.to(device)
+
+    # load data
+    test_df = pd.read_csv("api\\output\\output_validation_20260415071239.csv")
+
+    inputs = test_df["findings"].tolist()
+    references = test_df["labels"].tolist()
+
+    # predict
+    predictions = []
+
+    for text in inputs:
+
+        text = "summarize medical report and return summary in no more than 2 sentences: " + text
+
+        tokens = tokenizer(
+            text,
+            return_tensors="pt",
+            truncation=True,
+            max_length=256
+        ).to(device)
+
+        output = model.generate(
+            **tokens,
+            max_length=64,
+            num_beams=4
+        )
+
+        pred = tokenizer.decode(
+            output[0],
+            skip_special_tokens=True
+        )
+
+        predictions.append(pred)
+
+    print(predictions)
+    # rouge
+    rouge = evaluate.load("rouge")
+
+    results = rouge.compute(
+        predictions=predictions,
+        references=references
+    )
+
+    print(results)
--- api/evaluate/views.py
+++ api/evaluate/views.py
@@ -1,18 +1,46 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+import uuid
+import threading
+from .run_evaluation import run_evaluation_job
 # Create your views here.
 class EvaluateModelView(APIView):
+    JOB_STORE = {}
+    # simple in-memory store
+    def post(self, request):
+        
+        job_id = str(uuid.uuid4())
+        self.JOB_STORE[job_id] = {"status": "queued"}
 
-    def get(self, request):
+        thread = threading.Thread(
+            target=run_evaluation_job,
+            args=(job_id,)
+        )
+        thread.start()
+
+        return Response({
+            "job_id": job_id,
+            "status": "started"
+        })
+    
+    def get_status(self, request, job_id):
+        job = self.JOB_STORE.get(job_id)
+
+        if not job:
+            return Response({"error": "Job not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        return Response(job)
         # Here you would implement the logic to evaluate the model
         # For demonstration purposes, we'll just return a dummy evaluation result
-        evaluation_result = {
-            "accuracy": 0.95,
-            "precision": 0.92,
-            "recall": 0.93,
-            "f1_score": 0.925
-        }
+        # evaluation_result = {
+        #     "accuracy": 0.95,
+        #     "precision": 0.92,
+        #     "recall": 0.93,
+        #     "f1_score": 0.925
+        # }
+
         
-        return Response({"evaluation_result": evaluation_result}, status=status.HTTP_200_OK)
+        
+        # return Response({"evaluation_result": evaluation_result}, status=status.HTTP_200_OK)
   
